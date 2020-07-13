65fa664ff13febde07b02680fb0aaf0ae6b12ca2
MatthewFrench <9086152+MatthewFrench@users.noreply.github.com>
2020-07-12 23:23:34 -0500

    Added load game screen

--- js/load-game-window.js
+++ js/load-game-window.js
@@ -7,11 +7,26 @@ export class LoadGameWindow {
             this.hide()
             this.game.newGameWindow.show()
         }
+        this.gameContainer = document.getElementById("load-game-container")
     }
     show() {
         this.window.style.display = ""
     }
     hide() {
         this.window.style.display = "none"
+    }
+    add(gameName, gameLevel) {
+        let loadGameItem = document.createElement("button")
+        loadGameItem.classList.add("button")
+        loadGameItem.classList.add("load-game-item")
+        let name = document.createElement("div")
+        name.classList.add("load-game-item-name")
+        name.innerText = gameName
+        loadGameItem.append(name)
+        let level = document.createElement("div")
+        level.classList.add("load-game-item-level")
+        level.innerText = "Level " + gameLevel
+        loadGameItem.append(level)
+        this.gameContainer.append(loadGameItem)
     }
 }
--- js/new-game-window.js
+++ js/new-game-window.js
@@ -10,6 +10,12 @@ export class NewGameWindow {
         this.loadGameButton = document.getElementById("load-game-button")
         this.loadGameButton.onclick = () => {
             this.hide()
+            this.game.loadWindow.add("Eek", 1)
+            this.game.loadWindow.add("Bob", 5)
+            this.game.loadWindow.add("Ash Ketchum", 10)
+            this.game.loadWindow.add("Gary", 3)
+            this.game.loadWindow.add("I am awesome", 11)
+            this.game.loadWindow.add("He who shall not be named", 8)
             this.game.loadWindow.show()
         }
         this.aboutButton = document.getElementById("about-game-button")
