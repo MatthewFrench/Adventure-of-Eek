2c3845206f5b3147e1b87e4a385c6f444ced9cb6
Matthew French <9086152+MatthewFrench@users.noreply.github.com>
2020-12-23 21:25:06 -0600

    Creating an overworld in Tiled

---
+++ Tiled Project/Basic Tiles.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Basic Tiles" tilewidth="2048" tileheight="2048" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="2048" height="2048" source="../images/tiles/flowers.png"/>
+ </tile>
+ <tile id="1">
+  <image width="2048" height="2048" source="../images/tiles/grass.png"/>
+ </tile>
+ <tile id="2">
+  <image width="2048" height="2048" source="../images/tiles/house.png"/>
+ </tile>
+ <tile id="3">
+  <image width="2048" height="2048" source="../images/tiles/tree.png"/>
+ </tile>
+ <tile id="4">
+  <image width="2048" height="2048" source="../images/tiles/vines.png"/>
+ </tile>
+ <tile id="5">
+  <image width="2048" height="2048" source="../images/tiles/red.png"/>
+ </tile>
+</tileset>
