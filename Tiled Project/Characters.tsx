fa48cd49c58e8fef26cb82fd7a49b7b66aa77875
Matthew French <9086152+MatthewFrench@users.noreply.github.com>
2020-12-27 00:00:26 -0600

    Cleaned up originals vs game versions of graphics

--- Tiled Project/Characters.tsx
+++ Tiled Project/Characters.tsx
@@ -1,7 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Characters" tilewidth="200" tileheight="200" tilecount="1" columns="0">
+<tileset version="1.4" tiledversion="1.4.3" name="Characters" tilewidth="2048" tileheight="2048" tilecount="1" columns="0">
+ <editorsettings>
+  <export target="../map/Characters.json" format="json"/>
+ </editorsettings>
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="200" height="200" source="../images/baby-chicken.png"/>
+  <image width="2048" height="2048" source="../images/Originals/baby-chicken.png"/>
  </tile>
 </tileset>
